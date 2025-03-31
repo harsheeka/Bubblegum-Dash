4481d766687a3c6465e0c830959599fa519ced40
harsheeka <yoyoharshikaa@gmail.com>
2025-03-31 22:46:46 +0530

    added endless runner source code on vs

---
+++ Assets/Scripts/MainMenuControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControl : MonoBehaviour
+{
+    [SerializeField] GameObject fadeOut;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(StartButton());
+    }
+
+    IEnumerator StartButton()
+    {
+        fadeOut.SetActive(true);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(1);
+    }
+}
